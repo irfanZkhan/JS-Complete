8578b34c480cb36f95631272649abb1dbd745212
Irfan Haider <121978203+irfanZkhan@users.noreply.github.com>
2025-11-05 12:05:15 +0000

    changes

--- 06_Objects.js
+++ 06_Objects.js
@@ -25,5 +25,16 @@ const regularUser={
 const obj1={1:"irfan ", 2:"Haider ", 3:"khan"}
 const obj2={4:"Farhan ", 5:"Haider", 6:"Baloch" }
 
-const ObjectFinal= Object.assign(obj1,obj2)
-console.log (ObjectFinal)
+//const ObjectFinal= Object.assign({},obj1,obj2)//  why i put {} it will make sure add the both object 
+
+//use spread for merg two object 
+const ObjectMergSpreard={...obj1, ...obj2}
+//console.log (ObjectMergSpreard)
+
+
+
+console.log(TinderUser);
+
+console.log(Object.keys(TinderUser))
+console.log(Object.values(TinderUser))
+
