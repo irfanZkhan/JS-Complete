da61cbda7b013c1b19d4bfa7536028a02ed6167e
Irfan Haider <121978203+irfanZkhan@users.noreply.github.com>
2025-11-05 07:42:07 +0000

    changes

--- 06_Objects.js
+++ 06_Objects.js
@@ -3,4 +3,27 @@
 
 const TinderUser={}// this is simple object 
 
-console.log(TinderUser)
+
+TinderUser.UserName="Irfan Haider "
+TinderUser.id="abc123"
+TinderUser.Gmail="irfanhaider@gamil.com"
+
+
+const regularUser={
+    fullName:{
+        FirstNmae:"irfan",
+        LastName:"Haider"
+    }
+}
+
+//console.log(regularUser.fullName.LastName) 
+
+
+
+//how to merg or add two Objects
+ 
+const obj1={1:"irfan ", 2:"Haider ", 3:"khan"}
+const obj2={4:"Farhan ", 5:"Haider", 6:"Baloch" }
+
+const ObjectFinal= Object.assign(obj1,obj2)
+console.log (ObjectFinal)
