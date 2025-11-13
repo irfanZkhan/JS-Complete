8ec324564ab77fdaa8db4fc4603cc50a1a449e12
Irfan Haider <121978203+irfanZkhan@users.noreply.github.com>
2025-11-13 07:34:30 +0000

    changes

--- 07_Function.js
+++ 07_Function.js
@@ -54,14 +54,27 @@ const result =  AddtwoNumbers(12,24)
 
 
 
+// //value1 store 10 and value2 store 20 remaining all values in 
+// function CalculateCardPrice(value1, value2, ...num){
+//     // return value1,value2, num
+//     return num
 
-function CalculateCardPrice(value1, value2, ...num){
-    return num
-
-}
+// }
 
 // console.log("value1 = "  ,value1)
 // console.log("value2 = " ,value2)
-console.log(CalculateCardPrice(10, 20 ,30,40,60))
+//console.log(CalculateCardPrice(10, 20 ,30,40,60))
 
 
+const user={
+    userName:"iran haider ",
+    price: 999
+
+}
+
+function objectHandle(anyobject){
+    console.log(`this is a user name ${anyobject.userName} and this is the price ${anyobject.price}`)
+
+}
+
+objectHandle(user)
