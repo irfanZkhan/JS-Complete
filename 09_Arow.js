f968e94f69eaa18af96e4f256fd76bf67f702359
Irfan Haider <121978203+irfanZkhan@users.noreply.github.com>
2025-11-18 06:57:55 +0000

    changes

---
+++ 09_Arow.js
@@ -0,0 +1,16 @@
+// create a object 
+ const user={
+    userName: "irfan haider",
+    FatherName : "Munir Hussain ",
+    Email : "irfanhassam786@gamil.com",
+
+    WelcomMessage : function(){
+        console.log(`${this.userName} Welcome to this website `)
+        console.log(this)
+
+    }
+ }
+ user.WelcomMessage()
+ console.log(this)
+
+
